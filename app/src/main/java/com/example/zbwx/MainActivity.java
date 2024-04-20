0339949733d17e7cc1f19c653ba37610db031161
beiqimoren <601059534@qq.com>
2024-04-20 10:23:07 +0800

    4月20日早上，底部导航栏颜色搞定，运行正常版本

--- app/src/main/java/com/example/zbwx/MainActivity.java
+++ app/src/main/java/com/example/zbwx/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
 
         //text_user=findViewById(R.id.text_user);
         sp = this.getSharedPreferences("userInfo", Activity.MODE_PRIVATE);
-
         if(sp.getBoolean("AUTO_ISCHECK", false))
         {
             myApplication= (MyApplication) getApplication();
